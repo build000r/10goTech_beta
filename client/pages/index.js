45ab3198a533f88e16b5d2e351f108a41bc16087
Rob Baratta <robbaratta@Robs-MacBook-Pro.local>
2021-03-01 14:34:37 -0600

    u

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,51 +3,57 @@ import { useEffect } from "react";
 import Router from "next/router";
 
 const index = () => {
-  const subdomain =
-    typeof window !== "undefined" ? window.location.host.split(".")[0] : "";
-
-  const url =
-    typeof window !== "undefined"
-      ? window.location.host.split(".")[1].split(".")[0]
-      : undefined;
-
   const lookupSubdomainForCustomUrl = (url) => {
-    if (typeof url !== "undefined") {
-      console.log("URL received, ", url);
-
-      switch (url) {
-        case "hairytask" || "www.hairytask.com":
-          return "/user/vacation";
-        case "robbaratta" || "www.robbaratta.com":
-          return "/user/baratta";
-        case "wivud" || "www.wivud.com":
-          return "/user/vacation";
-        default:
-          return "/admin/rfp";
-      }
+    switch (url) {
+      case "hairytask" || "www.hairytask.com":
+        return "/user/vacation";
+      case "robbaratta" || "www.robbaratta.com":
+        return "/user/baratta";
+      case "wivud" || "www.wivud.com":
+        return "/user/vacation";
+      default:
+        return "/admin/rfp";
     }
   };
 
   useEffect(() => {
     let link;
 
-    switch (subdomain) {
-      case "rfp":
-        Router.push(`/admin/rfp`);
-        break;
-      case "10gotech":
-        Router.push(`/admin/rfp`);
-        break;
-      case "www":
-        link = lookupSubdomainForCustomUrl(url);
-        Router.push(link);
-        break;
-      default:
-        link = lookupSubdomainForCustomUrl(url);
-        Router.push("https://www." + subdomain + ".com" + link);
-        break;
+    const getSubdomain = () =>
+      typeof window !== "undefined"
+        ? window.location.host.split(".")[0]
+        : getSubdomain();
+
+    const getUrl = () =>
+      typeof window !== "undefined"
+        ? window.location.host.split(".")[1].split(".")[0]
+        : getUrl();
+
+    const subdomain = getSubdomain();
+    const url = getUrl();
+
+    console.log(subdomain, "subdomain");
+    console.log(url, "url");
+
+    if (typeof subdomain === "string") {
+      switch (subdomain) {
+        case "rfp":
+          Router.push(`/admin/rfp`);
+          break;
+        case "10gotech":
+          Router.push(`/admin/rfp`);
+          break;
+        case "www":
+          link = lookupSubdomainForCustomUrl(url);
+          Router.push(link);
+          break;
+        default:
+          link = lookupSubdomainForCustomUrl(url);
+          Router.push("https://www." + subdomain + ".com" + link);
+          break;
+      }
     }
-  }, [subdomain]);
+  }, []);
 
   return <Segment basic style={{ minHeight: "100vh" }} loading></Segment>;
 };
