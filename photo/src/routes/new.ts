b6e65aa039c113748cbe3b7d8e5406648c55fb6d
Rob Baratta <robbaratta@Robs-MacBook-Pro.local>
2021-03-21 18:44:47 -0600

    photo upload size

--- photo/src/routes/new.ts
+++ photo/src/routes/new.ts
@@ -18,7 +18,7 @@ import multer from "multer";
 
 const upload = multer({
   limits: {
-    fileSize: 1000000,
+    fileSize: 4000000,
   },
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
