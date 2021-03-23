c674940e833add12c9f675ddb652b830e2c6e75c
Rob Baratta <robbaratta@Robs-MacBook-Pro.local>
2021-03-23 13:51:23 -0400

    photos trycatch

--- photo/src/routes/view-by-product.ts
+++ photo/src/routes/view-by-product.ts
@@ -9,9 +9,13 @@ import { ProductImage } from "../models/image";
 const router = express.Router();
 
 router.get("/api/photos/:productId", async (req: Request, res: Response) => {
-  const images = await ProductImage.find({ productId: req.params.productId });
+  try {
+    const images = await ProductImage.find({ productId: req.params.productId });
 
-  res.status(200).send(images);
+    res.status(200).send(images);
+  } catch (error) {
+    res.send({ error });
+  }
 });
 
 export { router as viewPhotosByProductRouter };
