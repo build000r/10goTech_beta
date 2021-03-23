534fb7028750f6bd38a9578e808801704b6688c5
Rob Baratta <robbaratta@Robs-MacBook-Pro.local>
2021-03-23 13:43:16 -0400

    services trycatch

--- product/src/routes/index.ts
+++ product/src/routes/index.ts
@@ -6,18 +6,23 @@ import { Site } from "../models/site";
 const router = express.Router();
 
 router.get("/api/product/:siteTitle", async (req: Request, res: Response) => {
-  const site = await Site.findOne({ title: req.params.siteTitle });
-  if (!site) {
-    throw new NotFoundError();
+  try {
+    const site = await Site.findOne({ title: req.params.siteTitle });
+
+    if (!site) {
+      throw new NotFoundError();
+    }
+
+    const products = await Product.find({ site });
+
+    if (!products) {
+      throw new NotFoundError();
+    }
+
+    res.status(200).send(products);
+  } catch (error) {
+    res.send({ error });
   }
-
-  const products = await Product.find({ site });
-
-  if (!products) {
-    throw new NotFoundError();
-  }
-
-  res.status(200).send(products);
 });
 
 export { router as indexProductRouter };
