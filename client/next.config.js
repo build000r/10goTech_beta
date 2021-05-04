be50a15bbb07023610e31528a2d31899c04ef934
Rob Baratta <rjb@Robs-MacBook-Air.local>
2021-05-03 21:13:42 -0600

    ssr false email

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,24 @@ const withImages = require("next-images");
 
 module.exports = withImages({
   fileExtensions: ["jpg", "jpeg", "png", "gif"],
+
   webpackDevMiddleware: (config) => {
     config.watchOptions.poll = 300;
     return config;
   },
 });
+
+webpack = (config, options) => {
+  const externals = {
+    ...config.externals,
+    useQuill: "react-quilljs",
+  };
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: [...config.module.rules, ...rules],
+    },
+    externals,
+  };
+};
