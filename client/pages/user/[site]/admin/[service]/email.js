eeb4250b91bd3af0aeafe3e01ec2de580a3b7d65
Rob Baratta <rjb@Robs-MacBook-Air.local>
2021-05-03 21:06:19 -0600

    ssr false email

--- client/pages/user/[site]/admin/[service]/email.js
+++ client/pages/user/[site]/admin/[service]/email.js
@@ -1,17 +1,17 @@
 // create the email associated with this service
 
 import { buildClient } from "../../../../../api/build-client";
-// import UpdateOrderEmail from "../../../../../components/user-site/UpdateOrderEmail";
+import UpdateOrderEmail from "../../../../../components/user-site/UpdateOrderEmail";
 import Layout from "../../../../../components/layouts";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 import dynamic from "next/dynamic";
 
-const UpdateOrderEmail = dynamic(
-  () => import("../../../../../components/user-site/UpdateOrderEmail"),
-  { ssr: false }
-);
+// const UpdateOrderEmail = dynamic(
+//   () => import("../../../../../components/user-site/UpdateOrderEmail"),
+//   { ssr: false }
+// );
 
 const index = ({ email, ownerId }) => {
   const [renderClientSideComponent, setRenderClientSideComponent] = useState(
